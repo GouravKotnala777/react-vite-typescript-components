684e9695be717599c5b6073cbcb16af64b8a1b43
gouravkotnala <gouravkotnala777@gmail.com>
2026-01-04 22:49:30 +0530

    feat : add component for inserting code blocks

---
+++ src/components/CodeBlock.component.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from "react";
+import Prism from "prismjs";
+import "../styles/prism-custom-dracula.css";
+import "prismjs/components/prism-jsx";
+
+
+interface CodeBlockPropTypes{
+    code:string;
+    language:string;
+};
+
+function GenerateLines ({code}:{code:string}) {
+    return (
+        <>
+            {code.split("\n").map((_, i) => (
+            <div key={i} className="leading-6">
+                {i+1}
+            </div>
+            ))}
+        </>
+    )
+};
+
+function CodeBlock({code, language}:CodeBlockPropTypes) {
+    const preRef = useRef<HTMLElement|null>(null);
+
+    useEffect(() => {
+        if (!preRef.current) return;
+
+        Prism.highlightElement(preRef.current);
+    }, [code]);
+
+    return(
+        <div className="flex max-h-[300px] text-sm overflow-auto">
+  
+            {/* Line numbers */}
+            <div className="h-max select-none text-right pr-4 pl-3 bg-gray-200 text-gray-500 dark:bg-gray-800">
+                <GenerateLines code={code} />
+            </div>
+
+            {/* Code */}
+            <pre className="">
+                <code
+                    ref={preRef}
+                    className={`language-${language} whitespace-pre`}
+                >
+                    {code}
+                </code>
+            </pre>
+        </div>
+    )
+};
+
+export default CodeBlock;
