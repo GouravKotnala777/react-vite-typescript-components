72270abb74c918d370b612fc4a99d1ffd791f0bb
gouravkotnala <gouravkotnala777@gmail.com>
2026-01-06 15:40:12 +0530

    doc : add a new file for code strings

---
+++ src/utils/codeStrings.ts
@@ -0,0 +1,26 @@
+
+export const INPUT1_CODE = `
+    <div className="h-80 flex justify-center items-center">
+        <Input themeToggler={theme === "dark"} />
+    </div>
+`;
+export const NAVBAR_CODE = `
+    <div className="h-80 flex justify-center items-center">
+        <Navbar2 navItems={NAVITEMS} />
+    </div>
+`;
+export const NAVBAR2_CODE = `
+    <div className="h-80 flex justify-center items-center">
+        <Navbar navItems={NAVITEMS} padding="6px 12px" fontSize="18px" borderRadius="8px" previewGap="-60px" />
+    </div>
+`;
+export const NAVBAR3_CODE = `
+    <div className="h-80 flex justify-center items-center">
+        <Navbar3 navItems={NAVITEMS} blobTop="-10px" />
+    </div>
+`;
+export const NAVBAR4_CODE = `
+    <div className="h-80 flex justify-center items-center">
+        <Navbar4 navItems={NAVITEMS} marginTop="320px" />
+    </div>
+`;
