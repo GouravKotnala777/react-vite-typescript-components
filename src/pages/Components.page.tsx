3a58323f134dd39450e327a9b38c3e07d2bffcd9
gouravkotnala <gouravkotnala777@gmail.com>
2026-01-06 15:51:52 +0530

    refactor : update component page

--- src/pages/Components.page.tsx
+++ src/pages/Components.page.tsx
@@ -1,25 +1,21 @@
-import { type ReactNode } from "react";
-import AnimatedText from "../components/AnimatedText.component";
 import Navbar from "../components/Navbar.component";
-import TextStaggered from "../components/TextStaggered.component";
-import TooltipPanel from "../components/Tooltip.component";
-import BorderedImage from "../components/BorderedImage.component";
-import BorderGradientAnimation from "../components/BorderGradientAnimation.component";
 import Navbar2 from "../components/Navbar2.component";
 import Navbar3 from "../components/Navbar3.component";
 import Navbar4 from "../components/Navbar4.component";
 import Input from "../components/Input.component";
 import CodeBlock from "../components/CodeBlock.component";
 import Tab from "../components/Tab.component";
+import useTheme from "../hooks/useTheme";
+import HeadingPara from "../components/HeadingPara.component";
+import { INPUT1_CODE, NAVBAR2_CODE, NAVBAR3_CODE, NAVBAR4_CODE, NAVBAR_CODE } from "../utils/codeStrings";
 
-//const CODE_ICON = () => {
-//    return(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-//        <path stroke-linecap="round" stroke-linejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
-//    </svg>)
-//}
 const CODE_ICON = () => {
     return(
-        <path stroke-linecap="round" stroke-linejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+        <>
+            <path d="M8 9l3 3l-3 3"></path>
+            <path d="M13 15l3 0"></path>
+            <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+        </>
     )
 }
 const PREVIEW_ICON = () => {
@@ -28,16 +24,9 @@ const PREVIEW_ICON = () => {
             <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
             <path d="M6 8h.01"></path>
             <path d="M9 8h.01"></path>
-        </>
+        </>        
     )
 }
-//const PREVIEW_ICON = () => {
-//    return(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4">
-//        <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
-//        <path d="M6 8h.01"></path>
-//        <path d="M9 8h.01"></path>
-//    </svg>)
-//}
 
 
 const NAVITEMS:{
@@ -52,42 +41,18 @@ const NAVITEMS:{
     {iconPath:"M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9", text:"Signout", url:"/signout"}
 ];
 
-const code = `
-    // comment
-    const num = 10;
-    function test() {
-        return "Hello";
-    }
-`;
-const code2 = `
-    <div className="border border-blue-500">
-        <div className="border border-red-500 h-20 w-50">
-            <Input />
-        </div>
-        <div className="border border-red-500 h-20 w-100">
-            <Input width="210px" />
-        </div>
-        <div className="border border-red-500 h-20 w-150">
-            <Input width="260px" />
-        </div>
-        <div className="border border-red-500 h-20 w-200">
-            <Input width="310px" />
-        </div>
-    </div>
-`
-//const highlight = (code, language="markup") => {
-//    return Prism.highlight(code, Prism.languages[language], language)
-//}
 
 function Components() {
+    const {theme} = useTheme();
     
     return(
         <section className="max-w-3xl mx-auto min-h-screen mt-30 px-4">
 
-
-            <h2 className="text-2xl font-semibold mt-10">Input</h2>
-            <p className="tracking-normal leading-7 text-gray-600 dark:text-gray-200">A stylish dark mode supported navbar component-</p>
-
+            <HeadingPara
+                heading="Particle-Based Input Component"
+                para="A creative React input component where the typed text smoothly disintegrates into animated particles. This component demonstrates advanced DOM manipulation, canvas/animation logic, and React state handling without using any external animation libraries. Designed to be reusable, lightweight, and visually engaging."
+                url="/input"
+            />
             <Tab
                 panels={[
                     {
@@ -95,33 +60,119 @@ function Components() {
                         tabName:"Preview",
                         content:(
                             <div className="h-80 flex justify-center items-center">
-                                <Input />
+                                <Input themeToggler={theme === "dark"} />
                             </div>
                         )
                     },
                     {
                         tabIconPath:CODE_ICON(),
                         tabName:"Code",
-                        content:<CodeBlock code={code2} language="jsx" />
+                        content:<CodeBlock code={INPUT1_CODE} language="jsx" />
                     }
                 ]}
             />
 
-            <div className="border border-blue-500">
-                <div className="border border-red-500 h-20 w-50">
-                    <Input />
-                </div>
-                <div className="border border-red-500 h-20 w-100">
-                    <Input width="210px" />
-                </div>
-                <div className="border border-red-500 h-20 w-150">
-                    <Input width="260px" />
-                </div>
-                <div className="border border-red-500 h-20 w-200">
-                    <Input width="310px" />
-                </div>
-            </div>
 
+            <HeadingPara
+                heading="Navbar with Scroll-Synced Hover Indicator"
+                para="A modern navigation bar where a floating label box follows hovered icons, scrolling internally to display the relevant navigation text. Designed to deliver smooth micro-interactions and a clean user experience."
+                url="/navbar"
+            />
+            <Tab
+                panels={[
+                    {
+                        tabIconPath:PREVIEW_ICON(),
+                        tabName:"Preview",
+                        content:(
+                            <div className="h-80 flex justify-center items-center">
+                                <Navbar navItems={NAVITEMS} padding="6px 12px" fontSize="18px" borderRadius="10px" previewGap="-60px" />
+                            </div>
+                        )
+                    },
+                    {
+                        tabIconPath:CODE_ICON(),
+                        tabName:"Code",
+                        content:<CodeBlock code={NAVBAR_CODE} language="jsx" />
+                    }
+                ]}
+            />
+
+
+
+            <HeadingPara
+                heading="Animated Navbar Component"
+                para="A navbar with a popping up hover indicator that smoothly transitions across navigation items, delivering a modern and interactive navigation experience."
+                url="/navbar"
+            />
+            <Tab
+                panels={[
+                    {
+                        tabIconPath:PREVIEW_ICON(),
+                        tabName:"Preview",
+                        content:(
+                            <div className="h-80 flex justify-center items-center">
+                                <Navbar2 navItems={NAVITEMS} fontSize="15px" />
+                            </div>
+                        )
+                    },
+                    {
+                        tabIconPath:CODE_ICON(),
+                        tabName:"Code",
+                        content:<CodeBlock code={NAVBAR2_CODE} language="jsx" />
+                    }
+                ]}
+            />
+
+
+
+            <HeadingPara
+                heading="Interactive Gooey Navbar"
+                para="A visually engaging navbar with a fluid hover indicator that smoothly transitions across navigation items. The indicator adapts its position and content based on user interaction, delivering a modern and interactive navigation experience."
+                url="/navbar"
+            />
+            <Tab
+                panels={[
+                    {
+                        tabIconPath:PREVIEW_ICON(),
+                        tabName:"Preview",
+                        content:(
+                            <div className="h-80 flex justify-center items-center">
+                                <Navbar3 navItems={NAVITEMS} blobTop="-10px" />
+                            </div>
+                        )
+                    },
+                    {
+                        tabIconPath:CODE_ICON(),
+                        tabName:"Code",
+                        content:<CodeBlock code={NAVBAR3_CODE} language="jsx" />
+                    }
+                ]}
+            />
+
+
+            <HeadingPara
+                heading="Gooey Hover Navbar Component"
+                para="A visually engaging navbar with a fluid hover indicator that smoothly transitions across navigation items. The indicator adapts its position and content based on user interaction, delivering a modern and interactive navigation experience."
+                url="/navbar"
+            />
+            <Tab
+                panels={[
+                    {
+                        tabIconPath:PREVIEW_ICON(),
+                        tabName:"Preview",
+                        content:(
+                            <div className="h-80 flex justify-center items-center">
+                                <Navbar4 navItems={NAVITEMS} marginTop="320px" />
+                            </div>
+                        )
+                    },
+                    {
+                        tabIconPath:CODE_ICON(),
+                        tabName:"Code",
+                        content:<CodeBlock code={NAVBAR4_CODE} language="jsx" />
+                    }
+                ]}
+            />
 
 {/*<dialog>
 
@@ -296,16 +347,5 @@ function Components() {
         </section>
     )
 };
-
-
-function CodeWithComment({childElement, comment}:{childElement:ReactNode; comment:string;}) {
-    
-    return(
-        <div className="flex justify-between items-center gap-10 my-5 border-b border-gray-100">
-            {childElement}
-            <span className="text-green-600 text-xs">// props={comment}</span>
-        </div>
-    )
-}
 
 export default Components;
